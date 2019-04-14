45600d7bd4d5e09d7786d6087ac39b00d363ae34
wp360 <2897406527@qq.com>
2019-04-14 22:12:52 +0800

    完成短评

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -64,6 +64,36 @@ Page({
       posting: false
     })
   },
+  onPost(event) {
+    const comment = event.detail.text || event.detail.value
+    // const commentInput = event.detail.value
+    // 空内容判断
+    if(!comment) {
+      return
+    }
+    if(comment.length>12) {
+      wx.showToast({
+        title: '短评最多12个字',
+        icon: 'none'
+      })
+      return
+    }
+    bookModel.postComment(this.data.book.id, comment)
+      .then(res=>{
+        wx.showToast({
+          title: '+1',
+          icon: 'none'
+        })
+        this.data.comments.unshift({
+          content: comment,
+          nums: 1
+        })
+        this.setData({
+          comments: this.data.comments,
+          posting: false
+        })
+      })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
