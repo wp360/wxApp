87dc5d00c60282b26f245397c00936d3137cd605
wp360 <2897406527@qq.com>
2019-04-18 23:29:04 +0800

    搜索代码重构

--- components/search/index.js
+++ components/search/index.js
@@ -1,10 +1,16 @@
 // components/search/index.js
-import {KeywordModel} from '../../models/keyword'
+import {
+  KeywordModel
+} from '../../models/keyword'
 const keywordModel = new KeywordModel()
-import {BookModel} from '../../models/book'
+import {
+  BookModel
+} from '../../models/book'
 const bookModel = new BookModel()
 
-import {paginationBev} from '../behaviors/pagination'
+import {
+  paginationBev
+} from '../behaviors/pagination'
 
 Component({
   /**
@@ -47,54 +53,74 @@ Component({
   methods: {
     loadMore() {
       // console.log(this.data.q)
-      if(!this.data.q) {
+      if (!this.data.q) {
         return
       }
-      if(this.data.loading) {
+      if (this._isLocked()) {
         return
       }
       // 一次只发送一个请求
       // 锁
       // const length = this.data.dataArray.length
       // console.log(length)
-      if(this.hasMore()) {
-        this.data.loading = true
-        bookModel.search(this.getCurrentStart(),this.data.q).then(res=> {
+      if (this.hasMore()) {
+        this._locked()
+        // this.data.loading = true
+        bookModel.search(this.getCurrentStart(), this.data.q).then(res => {
           // const tempArray = this.data.dataArray.concat(res.books)
           this.setMoreData(res.books)
           // this.setData({
           //   dataArray: tempArray,
           //   // loading: false
           // })
-          this.data.loading = false
+          // this.data.loading = false
+          this._unLocked()
         })
       }
     },
     onCancel(event) {
-      this.triggerEvent('cancel',{},{})
+      this.triggerEvent('cancel', {}, {})
     },
     onDelete(event) {
-      this.setData({
-        searching: false
-      })
+      this._closeResult()
+      // this.setData({
+      //   searching: false
+      // })
     },
     onConfirm(event) {
-      this.setData({
-        searching: true
-      })
+      this._showResult()
       this.initialize()
       // 获取用户输入的关键字
       const q = event.detail.value || event.detail.text
       bookModel.search(0, q)
-      .then(res=>{
-        this.setMoreData(res.books)
-        this.setTotal(res.total)
-        this.setData({
-          // dataArray: res.books,
-          q
+        .then(res => {
+          this.setMoreData(res.books)
+          this.setTotal(res.total)
+          this.setData({
+            // dataArray: res.books,
+            q
+          })
+          keywordModel.addToHistory(q)
         })
-        keywordModel.addToHistory(q)
+    },
+    _showResult() {
+      this.setData({
+        searching: true
       })
+    },
+    _closeResult() {
+      this.setData({
+        searching: false
+      })
+    },
+    _isLocked() {
+      return this.data.loading ? true : false
+    },
+    _locked() {
+      this.data.loading = true
+    },
+    _unLocked() {
+      this.data.loading = false
     }
     // scroll-view | Page onReachBottom
   }
