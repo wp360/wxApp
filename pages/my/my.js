f4dfea9c729c992f477463e6a6acad0152976c5f
wp360 <2897406527@qq.com>
2019-04-21 14:51:04 +0800

    获取用户信息

--- pages/my/my.js
+++ pages/my/my.js
@@ -12,9 +12,37 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 旧的获取用户信息方法 wx.getUserInfo 授权弹窗
+    // wx.getUserInfo({
+    //   success: data => {
+    //     console.log(data)
+    //   }
+    // })
+    this.userAuthorized()
   },
-
+  userAuthorized() {
+    wx.getSetting({
+      success: data => {
+        // console.log(data)
+        if(data.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: data => {
+              console.log(data)
+            }
+          })
+        } else {
+          console.log('未授权')
+        }
+      }
+    })
+  },
+  getUserInfo(event) {
+    // console.log(event)
+  },
+  onGetUserInfo(event) {
+    const userInfo = event.detail.userInfo
+    console.log(userInfo)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
