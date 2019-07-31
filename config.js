f553fb490b274ec991a2cb191fc84b7ebdc1fbdc
wp360 <2897406527@qq.com>
2019-07-31 14:13:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
   api_base_url: 'http://bl.7yue.pro/v1/',
-  appkey: 'K5H3SiFRY9qLzs0n' //
+  appkey: 'MA0OKyXMxkLNEAIz' //K5H3SiFRY9qLzs0n
 }
 
 // export let fun = function() {
