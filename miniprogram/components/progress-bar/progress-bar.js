2e3dc485f072ebf6aff364dfbcf2b81b0444ff51
wp360 <2897406527@qq.com>
2019-12-29 15:17:33 +0800

    进度条布局

---
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -0,0 +1,124 @@
+// components/progress-bar/progress-bar.js
+let movableAreaWidth = 0
+let movableViewWidth = 0
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+let currentSec = -1 // 当前的秒数
+let duration = 0 // 当前歌曲的总时长，以秒为单位
+let isMoving = false // 表示当前进度条是否在拖拽，解决：当进度条拖动时候和updatetime事件有冲突的问题
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showTime: {
+      currentTime: '00:00',
+      totalTime: '00:00'
+    },
+    movableDis: 0,
+    progress: 0
+  },
+
+  // 生命周期
+  lifetimes: {
+    ready() {
+      this._getMovableDis()
+      this._bindBGMEvent()
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    _getMovableDis() {
+      const query = this.createSelectorQuery()
+      query.select('.movable-area').boundingClientRect()
+      query.select('.movable-view').boundingClientRect()
+      query.exec((rect) => {
+        console.log(rect)
+        movableAreaWidth = rect[0].width
+        movableViewWidth = rect[1].width
+        console.log(movableAreaWidth, movableViewWidth)
+      })
+    },
+    _bindBGMEvent() {
+      backgroundAudioManager.onPlay(() => {
+        console.log('onPlay')
+        isMoving = false
+        this.triggerEvent('musicPlay')
+      })
+
+      backgroundAudioManager.onStop(() => {
+        console.log('onStop')
+      })
+
+      backgroundAudioManager.onPause(() => {
+        console.log('Pause')
+        this.triggerEvent('musicPause')
+      })
+
+      backgroundAudioManager.onWaiting(() => {
+        console.log('onWaiting')
+      })
+
+      backgroundAudioManager.onCanplay(() => {
+        console.log('onCanplay')
+        // console.log(backgroundAudioManager.duration)
+        if (typeof backgroundAudioManager.duration != 'undefined') {
+          this._setTime()
+        } else {
+          setTimeout(() => {
+            this._setTime()
+          }, 1000)
+        }
+      })
+
+      backgroundAudioManager.onTimeUpdate(() => {
+        console.log('onTimeUpdate')
+      })
+
+      backgroundAudioManager.onEnded(() => {
+        console.log("onEnded")
+        this.triggerEvent('musicEnd')
+      })
+
+      backgroundAudioManager.onError((res) => {
+        console.error(res.errMsg)
+        console.error(res.errCode)
+        wx.showToast({
+          title: '错误:' + res.errCode,
+        })
+      })
+    },
+    _setTime() {
+      duration = backgroundAudioManager.duration
+      const durationFmt = this._dateFormat(duration)
+      console.log(durationFmt)
+      this.setData({
+        ['showTime.totalTime']: `${durationFmt.min}:${durationFmt.sec}`
+      })
+    },
+    // 格式化时间
+    _dateFormat(sec) {
+      // 分钟
+      const min = Math.floor(sec / 60)
+      sec = Math.floor(sec % 60)
+      return {
+        'min': this._parse0(min),
+        'sec': this._parse0(sec),
+      }
+    },
+    // 补零
+    _parse0(sec) {
+      return sec < 10 ? '0' + sec : sec
+    }
+  }
+})
