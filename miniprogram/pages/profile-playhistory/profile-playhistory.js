d9b0e63833a00d8b4b0a636205c6bf5b418ed9fe
wp360 <2897406527@qq.com>
2020-02-02 23:28:48 +0800

    播放历史

--- miniprogram/pages/profile-playhistory/profile-playhistory.js
+++ miniprogram/pages/profile-playhistory/profile-playhistory.js
@@ -6,14 +6,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    musiclist: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const playHistory = wx.getStorageSync(app.globalData.openid)
+    if(playHistory.length == 0) {
+      wx.showModal({
+        title: '播放历史为空',
+        content: ''
+      })
+    } else {
+      // storage里面存储的musiclist替换成播放历史里的歌单
+      wx.setStorage({
+        key: 'musiclist',
+        data: playHistory
+      })
+      this.setData({
+        musiclist: playHistory
+      })
+    }
   },
 
   /**
