cbb5c1a937515d7b00c09f50e4c8ff21bddee926
wp360 <2897406527@qq.com>
2020-04-25 16:13:48 +0800

    重构后台歌单列表显示接口

--- vue-admin-template/NOTE.md
+++ vue-admin-template/NOTE.md
@@ -366,10 +366,41 @@ const ENV = 'wxcloud-73wod'
 // ...
 
 app.use(async (ctx, next) => {
-  console.log('全局中间件')
+  // console.log('全局中间件')
   // ctx.body = 'Hello World'
   ctx.state.env = ENV
   await next()
 })
 
+```
+* 13. 重构后台歌单列表显示接口
+```js
+// vue-admin-template/server/controller/playlist.js
+const Router = require('koa-router')
+const router = new Router()
+// 引入封装的调用云函数方法
+const callCloudFn = require('../utils/callCloudFn')
+
+// get post
+router.get('/list', async(ctx, next) => {
+  // 获取前端属性值
+  const query = ctx.request.query
+  // 查询歌单列表
+  const res = await callCloudFn(ctx, 'music', {
+      $url: 'playlist',
+      start: parseInt(query.start),
+      count: parseInt(query.count)
+  })
+
+  let data = []
+  if(res.resp_data) {
+    data = JSON.parse(res.resp_data).data
+  }
+  ctx.body = {
+    data,
+    code: 20000
+  }
+})
+
+module.exports = router
 ```
--- vue-admin-template/server/controller/playlist.js
+++ vue-admin-template/server/controller/playlist.js
@@ -1,43 +1,28 @@
 // 歌单
 const Router = require('koa-router')
 const router = new Router()
-// 获取token值
-const getAccessToken = require('../utils/getAccessToken')
-const env = 'wxcloud-73wod'
-const rp = require('request-promise')
+// 引入封装的调用云函数方法
+const callCloudFn = require('../utils/callCloudFn')
 
 // get post
 router.get('/list', async(ctx, next) => {
-  const access_token = await getAccessToken()
-  // console.log(access_token)
-  // 查询歌单列表
-  const url = `https://api.weixin.qq.com/tcb/invokecloudfunction?access_token=${access_token}&env=${env}&name=music`
   // 获取前端属性值
   const query = ctx.request.query
-  const options = {
-    method: 'POST',
-    uri: url,
-    body: {
+  // 查询歌单列表
+  const res = await callCloudFn(ctx, 'music', {
       $url: 'playlist',
       start: parseInt(query.start),
       count: parseInt(query.count)
-    },
-    json: true
+  })
+
+  let data = []
+  if(res.resp_data) {
+    data = JSON.parse(res.resp_data).data
   }
-
-  const data = await rp(options)
-    .then((res) => {
-      // console.log(res)
-      return JSON.parse(res.resp_data).data
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
-    ctx.body = {
-      data,
-      code: 20000
-    }
+  ctx.body = {
+    data,
+    code: 20000
+  }
 })
 
 module.exports = router
